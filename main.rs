08820bc867902ff6ce5bdea9df93617fae593d73
reeckman <183706781+reeckman@users.noreply.github.com>
2025-03-31 12:55:26 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -28,7 +28,7 @@ async fn setup_environment() {
         ("NAME", "shuttle"),
         ("FILE_PATH", "./tmp"),
         ("ARGO_PORT", "8080"), // argo端口,
-        ("SUB_PATH", "cnnainai"), // 订阅路径
+        ("SUB_PATH", "sub"), // 订阅路径
     ];
 
     for (key, default_value) in env_vars {
